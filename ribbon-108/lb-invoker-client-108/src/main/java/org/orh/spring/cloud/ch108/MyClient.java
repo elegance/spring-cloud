9e81e84f9bcebd92aee54b75528ca35ac4a5df43
elegance <webdevelop@sohu.com>
2018-01-11 09:45:29 +0800

    delete others only stay ch-04-08

--- ribbon-108/lb-invoker-client-108/src/main/java/org/orh/spring/cloud/ch108/MyClient.java
+++
@@ -1,7 +0,0 @@
-package org.orh.spring.cloud.ch108;
-
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-
-//@RibbonClient(name = "service-provider", configuration = MyConfig.class)
-public class MyClient {
-}
